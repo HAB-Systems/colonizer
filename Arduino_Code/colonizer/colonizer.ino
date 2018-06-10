301e121b551fd2720d22b9d8964421b07c4407f5
Henry O'Callaghan <henry.ocallaghan@gmail.com>
2018-06-10 16:25:25 -0500

    Added more 'duino stuff

--- Arduino_Code/colonizer/colonizer.ino
+++ Arduino_Code/colonizer/colonizer.ino
@@ -1,20 +1,43 @@
 #include <Stepper.h>
 
+enum state{Standby, Running, Done} myState;
+
 const int stepsPerRevoloution = 200;
+const double stepsPerMMX = 10;
+const double stepsPerMMY = 10;
 
 Stepper xAxis(stepsPerRevoloution, 3, 4, 5, 6);
 Stepper yAxis(stepsPerRevoloution, 7, 8, 9, 10);
 
-
-
 void setup() {
   // put your setup code here, to run once:
-  switch(state):
-    {
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  switch (myState) {
+    case Running:
+      break;
+    case Done:
+      break;
+    default:
+      break;
+  }
 }
 
+double stepsToMM(int steps, int axis) {
+  return axis == 0 ? steps / stepsPerMMX : steps / stepsPerMMY;
+}
+
+double mmToSteps(double distance, int axis) {
+  return axis == 0 ? distance * stepsPerMMX : distance * stepsPerMMY;
+}
+
+void homeXAxis() {
+  
+}
+
+void homeYAxis() {
+  
+}
