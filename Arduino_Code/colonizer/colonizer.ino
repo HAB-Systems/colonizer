83898fca51b208dd47099da3cf2ee4c06328eac6
Henry O'Callaghan <henry.ocallaghan@gmail.com>
2018-06-10 15:43:49 -0500

    Started 'duino stuff

--- Arduino_Code/colonizer/colonizer.ino
+++ Arduino_Code/colonizer/colonizer.ino
@@ -1,13 +1,20 @@
 #include <Stepper.h>
 
+const int stepsPerRevoloution = 200;
+
+Stepper xAxis(stepsPerRevoloution, 3, 4, 5, 6);
+Stepper yAxis(stepsPerRevoloution, 7, 8, 9, 10);
+
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  switch(state):
+    {
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
-
 
