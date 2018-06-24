b7181cff03e13300c2202a1bbf4571195b043f5d
Henry O'Callaghan <henry.ocallaghan@gmail.com>
2018-06-23 21:05:00 -0500

    Added a multi-motor control

--- Arduino_Code/tester/tester.ino
+++ Arduino_Code/tester/tester.ino
@@ -1,34 +1,117 @@
-#define MOTOR_STEPS 200
-#define RPM 100
-
-#define DIR 55
-#define STEP 54
-#define ENABLE 38
-
-int MOTOR_ACCEL = 1000;
-int MOTOR_DECEL = 1000;
-#define MICROSTEPS 1
+typedef struct command
+{
+	enum actions {HOME, MOVE_FOR_CAMERA, PICKUP_COLONY, PLACE_COLONY, CUT_COLONY, HALT};
+	actions action;	
+	double xCoordinate;
+	double yCoordinate;
+}
 
 #include "A4988.h"
-#define MS1 10
-#define MS2 11
-#define MS3 12
-A4988 stepper(MOTOR_STEPS, DIR, STEP, ENABLE, MS1, MS2, MS3);
+
+#define X_MOTOR_STEPS 200
+#define X_RPM 100
+#define X_MICROSTEPS 1
+#define X_DIR 55
+#define X_STEP 54
+#define X_ENABLE 38
+#define X_MS1 10
+#define X_MS2 11
+#define X_MS3 12
+#define X_STEPS_MM 100
+
+#define Y_MOTOR_STEPS 200
+#define Y_RPM 100
+#define Y_MICROSTEPS 1
+#define Y_DIR 55
+#define Y_STEP 54
+#define Y_ENABLE 38
+#define Y_MS1 10
+#define Y_MS2 11
+#define Y_MS3 12
+#define Y_STEPS_MM 100
+
+#define Z_MOTOR_STEPS 200
+#define Z_RPM 100
+#define Z_MICROSTEPS 1
+#define Z_DIR 55
+#define Z_STEP 54
+#define Z_ENABLE 38
+#define Z_MS1 10
+#define Z_MS2 11
+#define Z_MS3 12
+#define Z_STEPS_MM 100
+
+#define E_MOTOR_STEPS 200
+#define E_RPM 100
+#define E_MICROSTEPS 1
+#define E_DIR 55
+#define E_STEP 54
+#define E_ENABLE 38
+#define E_MS1 10
+#define E_MS2 11
+#define E_MS3 12
+#define E_STEPS_MM 100
+
+const int X_ACCEL = 1000;
+const int X_DECEL = 1000;
+
+const int Y_ACCEL = 1000;
+const int Y_DECEL = 1000;
+
+const int Z_ACCEL = 1000;
+const int Z_DECEL = 1000;
+
+const int E_ACCEL = 1000;
+const int E_DECEL = 1000;
+
+bool xComplete = false;
+bool yComplete = false;
+bool zComplete = false;
+bool eComplete = false;
+bool 
+
+
+A4988 xMotor(X_STEPS, X_DIR, X_STEP, X_ENABLE, X_MS1, X_MS2, X_MS3);
+A4988 yMotor(Y_STEPS, Y_DIR, Y_STEP, Y_ENABLE, Y_MS1, Y_MS2, Y_MS3);
+A4988 zMotor(Z_STEPS, Z_DIR, Z_STEP, Z_ENABLE, Z_MS1, Z_MS2, Z_MS3);
+A4988 eMotor(E_STEPS, E_DIR, E_STEP, E_ENABLE, E_MS1, E_MS2, E_MS3);
+
+command currentCommand;
 
 void setup() {
-    stepper.begin(RPM, MICROSTEPS);
-    stepper.enable();
-    stepper.setSpeedProfile(stepper.LINEAR_SPEED, MOTOR_ACCEL, MOTOR_DECEL);
+	Serial.begin(9600);
+    xMotor.begin(X_RPM, X_MICROSTEPS);
+    yMotor.begin(Y_RPM, Y_MICROSTEPS);
+    zMotor.begin(Z_RPM, Z_MICROSTEPS);
+    eMotor.begin(E_RPM, E_MICROSTEPS);
+    xMotor.enable();
+    xMotor.setSpeedProfile(xMotor.LINEAR_SPEED, X_ACCEL, X_DECEL);
+    yMotor.enable();
+    yMotor.setSpeedProfile(yMotor.LINEAR_SPEED, Y_ACCEL, Y_DECEL);
+    zMotor.enable();
+    zMotor.setSpeedProfile(zMotor.LINEAR_SPEED, Z_ACCEL, Z_DECEL);
+    eMotor.enable();
+    eMotor.setSpeedProfile(eMotor.LINEAR_SPEED, E_ACCEL, E_DECEL);
 }
 
 void loop() {
-  if(!stepper.nextAction()) {
-    stepToIt();
-  }
+ 	if(Serial.available()) {
+		getCommand(&currentCommand);
+	}
+	if(!xMotor.nextAction()) {
+		xComplete = true;
+	}
+ 	if(!yMotor.nextAction()) {
+		yComplete = true;
+	}
+ 	if(!zMotor.nextAction()) {
+		zComplete = true;
+	}
+ 	if(!eMotor.nextAction()) {
+		eComplete = true;
+	}
 }
 
-void stepToIt() {
-  stepper.startRotate(360);
-  delay(500);
+void getCommand(command *c) {
+	
 }
-
